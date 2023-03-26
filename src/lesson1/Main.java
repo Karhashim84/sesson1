13f0c98a100afb2aead65cf9a1f62e69a46eda63
Karhashim <karhashim84@yandex.ru>
2023-03-26 22:26:06 +0500

    test

---
+++ src/lesson1/Main.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+import lesson1.task.Task1;
+import lesson1.task.task2;
+import lesson1.task.task3;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Task1 task1 = new Task1();
+//        Task1.hello();
+//        task2.time();
+          int[] array = new int[] {1,1,1,1,0,1,1,1,1,1,1};
+          System.out.println(task3.getMaxOneCount(array));
+    }
+}
